3edca86a651972000f989bae3208a895fd97b751
Daniele Fantini <danielefantini23@outlook.it>
2021-01-26 16:21:14 +0100

    Aggiungere i file di progetto.

---
+++ Models/BookViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCRUDwithoutEF.Models
+{
+    public class BookViewModel
+    {
+        [Key]
+        public int BookID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage ="Non deve esser più grande o uguale a 1")]
+        public int Price { get; set; }
+
+
+
+    }
+}
